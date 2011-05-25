5dae5aa8f37f8c254ad8f7e899dbac4d3d404830
Bhanukiran <bhanukiran.perabathini@gmail.com>
2011-05-25 17:06:44 +0530

    initial commit!

---
+++ Python/page11.py
@@ -0,0 +1,20 @@
+import scipy as sp
+import pylab as p
+#motion of a particle under the influence of a force F = -kx; k = 1
+
+def shm():
+    n = 100; j = 500
+    dt = 2*sp.pi/n
+
+    X = [0]*n
+    V = [1]*n
+    T = [0]*n
+    for i in xrange(0, n-1):
+        X[i+1] = V[i]*dt + X[i]
+        V[i+1] = -X[i]*dt + V[i]
+        T[i+1] = T[i] + dt
+
+    p.plot(T,X, 'x') + p.plot(T,V, 'o')
+    p.show()
+
+
